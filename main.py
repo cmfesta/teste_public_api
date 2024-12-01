77497d754954f6ac9ac2a563712f8f906ec039cd
unknown <guicmf97@gmail.com>
2024-12-01 15:40:55 -0300

    change

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_facebook():
     my_token = "abc123"  # The token you setup on the App dashboard
 
     if request.args.get("hub.verify_token") == my_token:
-        return int(datetime.datetime.now().timestamp())
+        return str(datetime.datetime.now().timestamp())
 
     return "invalid", 403
 
